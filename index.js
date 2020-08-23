277ab320c26ddd16c0218e4b82d78a3926952ec4
GiovannyRamirez <giovannyramirezs@hotmail.com>
2020-08-23 12:06:38 -0500

    https://github.com/GiovannyRamirez/SM-TEST.git

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 
 function suma(x, y) {
   // suma(2,3) // Retorna 5
+  return x+y;
   
 }
 
 function multiplica(x, y) {
   // multiplica(2,3) // Retorna 6
-  
+  return x*y;
 }
 
 function mayorQueTreinta(num) {
   // mayorQueTreinta(50) // Retorna true
-
+if (num<30) {
+  return false;
+} else {
+  return true;
+}
 }
 
 function saludo(idioma) {
@@ -20,13 +25,27 @@ function saludo(idioma) {
   // Si "idioma" es "mandarin", devuelve "Ni Hao!"
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
-
+if (idioma == 'aleman') {
+  return 'Guten Tag!'
+} else if (idioma == 'ingles') {
+  return 'Hello!'
+} else if (idioma == 'mandarin') {
+  return 'Ni Hao!'
+} else if (idioma == 'frances') {
+  return 'Hola!'
+} else {
+  return 'Hola!'
+}
 }
 
 function esEntero(numero) {
   // esEntero(50) // Retorna true
   // esEntero(0.8) // Retorna false
-
+if (numero%1 == 0) {
+  return true;
+} else {
+  return false;
+}
 }
 
 function fizzBuzz(numero) {
@@ -34,11 +53,22 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
-
+if (numero%5 == 0 && numero%3 == 0) {
+  return "fizzbuzz"
+}
+if (numero%3 == 0) {
+  return "fizz"
+}
+if (numero%5 == 0) {
+  return "buzz"
+} else {
+  return numero;
+}
 }
 
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
+  return array.length;
   
 }
 
@@ -47,18 +77,42 @@ function dePalabrasAFrase(palabras) {
   // Devuelve un string donde todas las palabras estén concatenadas
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
-
+pal=palabras.join(" ");
+return pal;
 }
 
 function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
-
+let sum=resultadosTest.reduce((previous, current)=>current+=previous);
+let prom=sum/(resultadosTest.length);
+return prom;
 }
 
 function esPrimo(numero) {
   // Devuelve "true" si "numero" es primo
   // De lo contrario devuelve "falso"
+  var cant_div=0;
+    if (numero==0) {
+       return false
+    }
+    else {
+        for (var i=1; i<=numero; i++){
+        // Verificación del residuo
+        var residuo=numero%i;
+            if (residuo==0) {
+                // Sumar 1 a la cantidad de divisores e impresión
+                cant_div=cant_div+1;
+            }
+        }
+        // Verificación de número primo
+        if (numero>1&&cant_div==2){
+          return true
+        }
+        else {
+          return false;
+        }
+    }
 }
 
 
